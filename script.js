fa7676a080758761d044ed12c4d379346611ddb7
Weber <wliu1996@hotmail.com.au>
2020-01-03 20:29:49 +1100

    dynamic slide timings

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-function loadSlide(slideName) {
-  slideData.map(item => {
-    if (item.slideName == slideName) {
-      viewer.open(item.imgLoc);
-      video.src = item.videoLoc;
-      document.getElementById("slideInfo").innerHTML = item.slideDescription;
-      document
-        .getElementById("videoTab")
-        .getElementsByClassName("mydivheader")[0].innerHTML = item.videoTitle;
-      document
-        .getElementById("histology")
-        .getElementsByClassName("mydivheader")[0].innerHTML = item.imgTitle;
-      console.log("Successfully loaded new slide");
-    }
-  });
-
-  // alert(
-  //   "You clicked on slide " +
-  //     slideName +
-  //     " however we currently don't have that slide available"
-  // );
-}
+function loadSlide() {
+  viewer.open(slideData.imgLoc);
+  video.src = slideData.videoLoc;
+  document.getElementById("slideInfo").innerHTML = slideData.slideDescription;
+  console.log("Successfully loaded new slide");
+};
 
 function slidePosition() {
-  if (this.currentTime > 2 && this.currentTime < 3) {
-    moveViewPort(0.3, 0.3, 5);
-  } else if (this.currentTime > 5 && this.currentTime < 6) {
-    moveViewPort(0.7, 0.1, 2);
-  } else if (this.currentTime > 10 && this.currentTime < 11) {
-    moveViewPort(0.7, 0.19, 5);
-  }
+  slideData.slideTimings.map(item => {
+    var timeData = item.split("&").map(Number)
+    if (this.currentTime - timeData[0] < 1) {
+      moveViewPort(timeData[1], timeData[2], timeData[3]);
+    }
+  })
 }
 
 // do not change anything after this
@@ -36,7 +19,7 @@ function slidePosition() {
 var viewer = OpenSeadragon({
   id: "openseadragon",
   prefixUrl: "./openseadragon-bin-2.4.1/images/",
-  tileSources: "./blue-12/blue-0.xml"
+  tileSources: ""
 });
 var viewPort = viewer.viewport;
 
@@ -44,10 +27,9 @@ window.onload = function () {
   video = document.createElement("video");
   video.setAttribute("id", "Myvideo");
   video.setAttribute("controls", "controls");
-  video.src = "./Histology slide A/videoA/Histology of the Kidney.mp4";
   video.addEventListener("timeupdate", slidePosition, false);
-  document.getElementById("myVideo").appendChild(video);
-  loadSlide("ye17");
+  document.getElementById("slideVideo").appendChild(video);
+  loadSlide();
 };
 
 function moveViewPort(posX, posY, zoomDeg) {
@@ -56,6 +38,8 @@ function moveViewPort(posX, posY, zoomDeg) {
 }
 
 console.log(slideData);
+var timingData = slideData['slideTimings'][0].split("&").map(Number)
+console.log(timingData)
 
 //function to output points on the slide
 var update_points = setInterval(function () {
--- slidedata.js
+++ slidedata.js
@@ -1,19 +1,21 @@
-slideData = [
+slideData =
   {
-    slideName: "ye17",
-    imgTitle: "blue-12 oesophagus",
+    slideName: "BLUE-12",
     imgLoc: "./blue-12/blue-0.xml",
-    videoTitle: "Histology video - RCC",
     videoLoc: "./Histology slide A/videoA/Histology of the Kidney.mp4",
     slideDescription:
-      "<b>Renal cell carcinoma</b> <br/> This is histological slide of the kidney. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-  },
-  {
-    slideName: "br17",
-    imgTitle: "",
-    imgLoc: "",
-    videoTitle: "",
-    videoLoc: "",
-    slideDescription: "Slide does not yet exist!"
+      "<b>Blue-12 Oesophagus</b> <br/> " +
+      "This is histological slide of the kidney. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    slideMetadata: {
+      Author: "Weber Liu",
+      Presenter: "Weber Liu",
+      Editor: "Weber Liu",
+      Copyright: "Slide copyright University of Sydney"
+    },
+    slideTimings: {
+      0: "0054 & 100 & 0.200 & 300",
+      1: "No",
+      2: "Third item"
+    }
   }
-];
+  ;
